1dd04288e9b0a38c91adc03469f762f2dbc15d98
jimbona <90354463+jimbona@users.noreply.github.com>
2026-06-29 01:24:09 -0400

    0.1.1
    Updated Textures, added World scene, updated player animations and move script

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,25 +1,41 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 85
+@onready var animated_sprite = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(_delta):
+	
+	var animation_to_play = ""
+	
+	if Input.is_action_pressed("move_right"):
+		animation_to_play = "walk_right"
+		velocity.x = move_speed
+	elif Input.is_action_pressed("move_left"):
+		animation_to_play = "walk_left"
+		velocity.x = -move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+	
+	if Input.is_action_pressed("move_down"):
+		animation_to_play = "walk_down"
+		velocity.y = move_speed
+	elif Input.is_action_pressed("move_up"):
+		animation_to_play = "walk_up"
+		velocity.y = -move_speed
+	else:
+		velocity.y = 0
+	
+	if velocity.length() > 10:
+		animated_sprite.play(animation_to_play)
+	else:
+		match animated_sprite.animation:
+			"walk_right", "idle_right":
+				animated_sprite.play("idle_right")
+			"walk_left", "idle_left":
+				animated_sprite.play("idle_left")
+			"walk_up", "idle_up":
+				animated_sprite.play("idle_up")
+			"walk_down", "idle_down":
+				animated_sprite.play("idle_down")
+	
 	move_and_slide()
